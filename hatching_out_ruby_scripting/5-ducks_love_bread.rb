d37f6c0cbdb403c225f2999e0ae4fc5533b5436f
Gloria Bwandungi <30@holbertonschool.com>
2016-03-16 03:28:13 -0400

    fifth file edited to change dirs

--- hatching_out_ruby_scripting/5-ducks_love_bread.rb
+++ hatching_out_ruby_scripting/5-ducks_love_bread.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 
 # This program has been written to take a directory as an argument
-# and print out all the files that match a given string. 
+# and print out all the files that match a given string.
 
-ARGV.each do
+ARGV.each do|a|
+  Dir.chdir "#{a}"
   puts Dir["*bread*"] #=> In this case, it takes the string 'bread'
 end
